369ab424ff38eb37122503a60686790eac94050b
Taiki Nakatsuka <gndpig@gndpig.local>
2012-10-08 01:43:32 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '524369d2d0ae0eec59115d3e14e591b96a704c4dc389dd58d9e43197ef8031dd35b570bdf128f34be5d154bd9b0fdd9530e2439828d469d85aaaed462ab9b674'
